fc7ff95fbb6e6ecdf8057e6ac07a0857920430dc
equli <mtomaszewski006@gmail.com>
2024-04-06 16:02:53 +0200

    Add changes

--- src/CV/index.tsx
+++ src/CV/index.tsx
@@ -2,13 +2,34 @@ import './CV.scss'
 import {Personal} from "../Personal";
 import {Details} from "../Details";
 import {Header} from "../Header";
+import {CVData} from "../types.ts";
 
 
 const CV = () => {
+    const cvData: CVData = {
+        personal: {
+            photo: 'https://placehold.co/400x400',
+            name: 'Maciej',
+            lastName: 'Tomaszewski',
+            position: 'Developer',
+        },
+        details: {
+        experience: [{
+            year: 2020,
+            description:  "lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum"
+        },{
+            year: 2023,
+            description: "Ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum"
+        }],
+        education: [
+            "Wyższa szkoła",
+        ]
+        }
+    }
     return <main className='cv-main'>
-        <Header/>
-        <Personal/>
-        <Details/>
+        <Header data={cvData.personal}/>
+        <Personal data={cvData.personal}/>
+        <Details data={cvData.details}/>
     </main>
 }
 
